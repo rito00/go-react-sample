322da7963bc0a68247e8fbe8f556fc1b8e38e27c
R.Ito <iriku.ir@gmail.com>
2024-07-19 17:24:28 +0900

    backend 給水履歴表示のためのAPI追加

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -20,8 +21,16 @@ type Plant struct {
 	StateType string `json:"state_type"`
 }
 
+type WateringHistory struct {
+	PlantID            int       `json:"plant_id"`
+	WateringDate       time.Time `json:"watering_date"`
+	FertilizerRecipeID int       `json:"fertilizer_recipe_id"`
+	Amount             float64   `json:"amount"`
+	AdditionalInfo     string    `json:"additional_info"`
+}
+
 func main() {
-	db, err := sql.Open("mysql", "kabu:kabu@tcp(db:3306)/kabu_db")
+	db, err := sql.Open("mysql", "kabu:kabu@tcp(db:3306)/kabu_db?parseTime=true")
 	if err != nil {
 		log.Fatal("Database connection error: ", err)
 	}
@@ -35,6 +44,7 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/api/plants", getPlants(db)).Methods("GET")
+	router.HandleFunc("/api/watering-history", getWateringHistory(db)).Methods("GET")
 
 	// CORS の設定
 	c := cors.New(cors.Options{
@@ -92,4 +102,62 @@ func getPlants(db *sql.DB) http.HandlerFunc {
 	}
 }
 
+func getWateringHistory(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var wateringHistory []WateringHistory
+		var query string
+		var args []interface{}
+		plantID := r.URL.Query().Get("plant_id")
 
+		if plantID != "" {
+				query = `
+						SELECT p.plant_id, p.location_id, w.watering_date, w.amount, f.recipe_name
+						FROM plants p
+						JOIN watering_history w ON p.plant_id = w.plant_id
+						LEFT JOIN fertilizer_recipes f ON w.fertilizer_recipe_id = f.recipe_id
+						WHERE p.plant_id = ? 
+						ORDER BY w.watering_date DESC;
+				`
+				args = append(args, plantID)
+		} else {
+				query = `
+						SELECT p.plant_id, p.location_id, w.watering_date, w.amount, f.recipe_name
+						FROM plants p
+						JOIN watering_history w ON p.plant_id = w.plant_id
+						LEFT JOIN fertilizer_recipes f ON w.fertilizer_recipe_id = f.recipe_id
+						ORDER BY w.watering_date DESC;
+				`
+		}
+
+		rows, err := db.Query(query, args...)
+		if err != nil {
+			log.Printf("Database query error: %v", err)
+			http.Error(w, "Internal server error1", http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+		
+		for rows.Next() {
+			var wh WateringHistory
+			if err := rows.Scan(&wh.PlantID, &wh.FertilizerRecipeID, &wh.WateringDate, &wh.Amount, &wh.AdditionalInfo); err != nil {
+				log.Printf("Row scan error: %v", err)
+				http.Error(w, "Internal server error2", http.StatusInternalServerError)
+				return
+			}
+			wateringHistory = append(wateringHistory, wh)
+		}
+		
+		if err := rows.Err(); err != nil {
+			log.Printf("Row iteration error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(wateringHistory); err != nil {
+			log.Printf("JSON encoding error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+}
